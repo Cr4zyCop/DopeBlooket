78306f38fd5cd3c46e085a04e6fda4208ec0310c
Cr4zyCop <110054319+Cr4zyCop@users.noreply.github.com>
2023-12-07 17:36:50 -0600

    Update mobileGui.js

--- cheats/mobileGui.js
+++ cheats/mobileGui.js
@@ -162,7 +162,7 @@
         bodyDiv.appendChild(footer);
         footer.style.fontSize = '0.9rem';
         footer.style.paddingBottom = '5px';
-        footer.innerHTML = (`<span><a target="blank" href="https://discord.gg/jHjGrrdXP6">Discord</a><br/><a target="blank" href="https://github.com/Cr4zyCop/DopeBlooket">GitHub - Cr4zyCop At Dope Unbl0cked Games</a></span>`);
+        footer.innerHTML = (`<span><a target="blank" href="https://discord.gg/jHjGrrdXP6">Discord</a><br/><a target="blank" href="https://github.com/Cr4zyCop/DopeBlooket">GitHub - Cr4zyCop At Dope</a></span>`);
         
         let cheats = ({
             global: [
